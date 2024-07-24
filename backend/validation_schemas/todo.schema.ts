ad8e44a0666949f6b00be44d7e5353ca58d8ae2a
Henrique <henrimaj@gmail.com>
2024-07-24 19:17:43 -0300

    feat/frontend/adding-table

--- backend/validation_schemas/todo.schema.ts
+++ backend/validation_schemas/todo.schema.ts
@@ -3,8 +3,9 @@ const { z } = require('zod');
 const TodoSchema = z.object({
   id: z.string().optional(), 
   title: z.string().min(1, { message: "Title must not be empty" }), 
-  description: z.string().optional(), 
-  completed: z.boolean(),
+  task: z.string().optional(), 
+  status: z.enum(["BACKLOG", "TODO","PROGRESS","DONE","CANCELED"]).optional().default('BACKLOG'),
+  priority: z.enum(["MEDIUM", "HIGH","LOW"]).optional().default('LOW'),
   createdAt: z.date().optional(), 
   updatedAt: z.date().optional(), 
 });
